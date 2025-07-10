d00b1a446443abadb1b36d24b5b4ac62978662f4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-10 16:39:11 -0400

    Created a footer componentand applied globally to the main layout.

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-gray-200">
+        <div className="overflow-hidden  shadow sm:rounded-lg">
+          <div className="px-4 py-5 sm:p-6">
+            <p>This is a headinsg</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
