4d883f16d541e6c9c9f7b90d78ee3199f6ae9634
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-10 16:43:48 -0400

    Created a paged header to help users identify the page their on.

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,17 @@
+export default function Resume() {
+  return (
+    <>
+      <main className="min-h-dvh container mx-auto mt-24">
+        <section className="max-w-5xl">
+          <h1 className="scroll-m-20 text-4xl  lg:tracking-wide font-extrabold tracking-tight">
+            Build Your Resume
+          </h1>
+          <p className="text-lg/[2] tracking-wide mt-4">
+            The king, seeing how much happier his subjects were, realized the
+            error of his ways and repealed the joke tax.
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
