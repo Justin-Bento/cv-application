1b6f40ba743e8653592202c562e1c7974eee0d3e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-10 17:17:51 -0400

    Updated content for webpage.

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -3,12 +3,12 @@ export default function Resume() {
     <>
       <main className="min-h-dvh container mx-auto mt-24">
         <section className="max-w-5xl">
-          <h1 className="scroll-m-20 text-4xl  lg:tracking-wide font-extrabold tracking-tight">
+          <h1 className="scroll-m-20 text-4xl lg:tracking-wide font-extrabold tracking-tight">
             Build Your Resume
           </h1>
           <p className="text-lg/[2] tracking-wide mt-4">
-            The king, seeing how much happier his subjects were, realized the
-            error of his ways and repealed the joke tax.
+            Create a professional resume that stands out. Edit in real-time and
+            download as PDF.
           </p>
         </section>
         <section className="grid grid-cols-2 gap-8 mt-8">
