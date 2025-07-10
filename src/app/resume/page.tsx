7a47f8fd6ec07d14848b3ced26137858c0491a7a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-10 17:18:16 -0400

    Created a new section to host generate pdf at the end of the website.

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,3 +1,5 @@
+import { Download } from "lucide-react";
+
 export default function Resume() {
   return (
     <>
@@ -14,6 +16,15 @@ export default function Resume() {
         <section className="grid grid-cols-2 gap-8 mt-8">
           <div className="rounded-xl border py-6">&nbsp;</div>
           <div className="rounded-xl border py-6 shadow-sm">&nbsp;</div>
+        </section>
+        <section className="mt-12">
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 flex items-center gap-1.5"
+          >
+            <Download className="w-5 h-5" />
+            Generate PDF
+          </button>
         </section>
       </main>
     </>
