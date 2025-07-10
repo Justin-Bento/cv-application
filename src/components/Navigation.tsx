6aca86ea0257a5a0ae07b4f2ecd95768d3f90a1f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-10 16:37:25 -0400

    Added card to header application to show header is elivated above the content

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function Navigation() {
   return (
     <>
-      <main className="">
-        <p>This is a headinsg</p>
+      <main className=" sticky top-0 bg-white">
+        <div className="overflow-hidden  shadow sm:rounded-lg">
+          <div className="px-4 py-5 sm:p-6">
+            <p>This is a headinsg</p>
+          </div>
+        </div>
       </main>
     </>
   );
