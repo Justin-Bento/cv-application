89a3e98ccc1aeca3764e2131793223a0f2883cbc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-10 16:19:27 -0400

    Started using tailwind inside project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1 className="">Hello, Next.js.</h1>;
+  return (
+    <>
+      <main className="">
+        <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      </main>
+    </>
+  );
 }
