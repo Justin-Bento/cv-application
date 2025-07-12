cd8d519bec84286fd05867e310694c62794e63f7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-11 20:02:41 -0400

    Added document icon from iconsvg

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,23 @@ export default function Navigation() {
       <main className=" sticky top-0 bg-white">
         <div className="overflow-hidden  shadow sm:rounded-lg">
           <div className="px-4 py-5 sm:p-6">
-            <p className="uppercase text-sm font-bold tracking-wider">This is a headinsg</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" />
+              <path d="M14 3v5h5M16 13H8M16 17H8M10 9H8" />
+            </svg>
+            <p className="uppercase text-sm font-bold tracking-wider">
+              This is a headinsg
+            </p>
           </div>
         </div>
       </main>
