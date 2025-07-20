c59771c838a865bb3894ea430863b885d29090ff
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-19 21:52:12 -0400

    Applied wrapper styles to main html element

--- src/app/cover-letter/page.tsx
+++ src/app/cover-letter/page.tsx
@@ -1,7 +1,7 @@
 export default function CoverLetter() {
   return (
     <>
-      <main className="">
+      <main className="min-h-dvh container mx-auto my-24">
         <section className="">
           <h1 className="">Build Your Cover Letter</h1>
           <p className="">
