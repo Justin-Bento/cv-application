f2c1c9e7758cbeb0cd3581e8691eb3260d1e0f24
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-19 21:51:32 -0400

    Created a new page called cover letter.

---
+++ src/app/cover-letter/page.tsx
@@ -0,0 +1,14 @@
+export default function CoverLetter() {
+  return (
+    <>
+      <main className="">
+        <section className="">
+          <h1 className="">Build Your Cover Letter</h1>
+          <p className="">
+            Create a professional Cover Letter that stands out. Edit in real-time and download as PDF.
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
